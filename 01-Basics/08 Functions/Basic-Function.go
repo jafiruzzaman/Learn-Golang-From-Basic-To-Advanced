5c7073e0de16f57a2a0bea162f9d6a0a17291a2b
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-09 17:49:37 +0600

    learn how to declare and call function In Go-lang

---
+++ 01-Basics/08 Functions/Basic-Function.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Function Declaration
+func greet() {
+	fmt.Println("Hey, Go-lang!")
+}
+
+
+func main(){
+	fmt.Println("========================================================== Basic Functions In Go-lang ==========================================================")
+	greet()// Call greet function
+}
