23a939d79060fadcecb0c18ba985c393835311cd
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-09 16:24:20 +0600

    go program structure

---
+++ 01-Basics/02 Program-Structure/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("===================================== Go Program Structure =====================================")
+}
+
+/*
+	* package main  -> Defines the main package (required for executable programs)
+	* import "fmt"  -> Imports the fmt package for formatted I/O
+	* func          -> Keyword to declare a function
+	* main          -> Entry point of the Go program
+	* fmt.Println  -> Prints output to the console with a newline
+*/
